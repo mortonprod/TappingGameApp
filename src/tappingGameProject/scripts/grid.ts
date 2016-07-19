9d0761173c03d6aa161e25cb77882d07a1d53cd3
Alexander Morton <alexander.morton975@gmail.com>
2016-07-19 13:04:37 +0100

    Add a constant loop to grid to trigger tapping on sqrs

--- src/tappingGameProject/scripts/grid.ts
+++ src/tappingGameProject/scripts/grid.ts
@@ -1,53 +1,78 @@
-﻿///The grid is composed of tap-sqr components.
-///Should implementthe composite design pattern.
-
-import {Output,Component, OnInit, OnDestroy} from '@angular/core';
+﻿///The grid deals with specifying which blocks to fire.
+import {Output, Component, OnInit, OnDestroy, EventEmitter, Input, SimpleChange} from '@angular/core';
 import {TapSqr} from './sqr';
+import {TapStatements} from './statements';
+
 
 @Component({
     selector: "tap-grid",
-    directives:[TapSqr],
+    directives: [TapSqr, TapStatements],
     templateUrl: '../MyStaticFiles/grid.html'
 })
 ///Always implement class with some basic angular functionality.
 export class TapGrid implements OnInit, OnDestroy {
-    ///If in title sequence then we want to run some default animation in the background.
-    isTitleGrid: boolean = false;
-    isTitle: Array<boolean> = new Array();
+    @Input() isTitle: boolean = true;
+    @Output() tapped = new EventEmitter();
+    states: Array<string> = new Array();
+    statementState: string = "initial";
+    isStatement: boolean = false;
+    missed: number = 0;
+    got: number = 0;
+    time: number = 2000;
+    fireNum: number = 2;
     ngOnInit() { }
     ngOnDestroy() {  }
     constructor() {
-        for (let i: number = 0; i < 17; i++) {
-            this.isTitle.push(false);
-        }
-        setTimeout(() => {
-            this.isTitle[0] = true;
-            this.isTitle[1] = true;
-            this.isTitle[2] = true;
-            this.isTitle[3] = true;
-        }, 2000);
-        setTimeout(() => {
-            this.isTitle[4] = true;
-            this.isTitle[5] = true;
-            this.isTitle[6] = true;
-            this.isTitle[7] = true;
-        }, 3000);
-        setTimeout(() => {
-            this.isTitle[8] = true;
-            this.isTitle[9] = true;
-            this.isTitle[10] = true;
-            this.isTitle[11] = true;
-        }, 4000);
-        setTimeout(() => {
-            this.isTitle[12] = true;
-            this.isTitle[13] = true;
-            this.isTitle[14] = true;
-            this.isTitle[15] = true;
-        }, 5000);
-    }
-    public sqrTapped() {
-        for (let i: number = 0; i < 17; i++) {
-            this.isTitle[i] = false;
+        for (let i = 0; i < 17; i++) {
+            this.states.push("initial");
+            console.log("state: " + this.states[i]);
         }
     }
+    ngOnChanges(changes: { [isTitle: string]: SimpleChange }) {
+        if (!this.isTitle) {
+            setInterval(() => {
+                for (let i = 0; i < this.fireNum; i++) {
+                    this.states[Math.floor(Math.random() * this.states.length)] = "toBeClicked";
+                }
+                console.log("Fire loop");
+            }, 2000)
+        }
+    }
+
+
+    ///Logic to deal with 
+    public sqrTapped(event) {
+        if (event[0] == "missed") {
+            this.missed = this.missed + 1;
+            console.log("Missed: " + this.missed);
+        } 
+        if (event[0] == "gotIt") {
+            this.got = this.got + 1;
+            console.log("Got: " + this.got);
+        } 
+        if (this.missed > 5) {
+            this.isStatement= true;
+            this.statementState = "missed";
+            setTimeout(() => {
+                this.isStatement = false;
+            }, this.time)
+        }
+        if (this.got > 5) {
+            this.isStatement = true;
+            this.statementState = "gotIt";
+            setTimeout(() => {
+                this.isStatement = false;
+            }, this.time)
+        }
+        ///Pass got and missed to application. 
+        this.tapped.next([this.got,this.missed]);
+    }
+    ///We call a function constantly from the grid class to fire the sqrs below.
+    private loop() {
+        for (let i = 0; i < this.fireNum; i++) {
+            this.states[Math.floor(Math.random() * this.states.length)] = "isToBeClicked";
+        }
+        console.log("Fire loop");
+        setTimeout(this.loop, this.time);
+    };
 }
