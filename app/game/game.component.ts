ea5b91a7731126510defa1300bd61cae97aa0c21
mortonprod <mortonprod@gmail.com>
2016-08-05 02:48:28 +0100

    Add game component
    This is the component which holds the grid and the sqr.
    It should anything which is in the grid area

---
+++ app/game/game.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+import { TapGrid } from './grid'
+ @Component({
+    selector: "tap-game",
+    directives: [TapGrid],
+    templateUrl:'./game.template.html'
+})
+
+export class TapGame {
+}
