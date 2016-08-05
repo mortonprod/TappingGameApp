a58a7f29c0bb7ce75671f5a4f57fc2fc8a9f6f9c
mortonprod <mortonprod@gmail.com>
2016-08-05 02:49:51 +0100

    Add grid and sqr components
    This is the grid and sqrs which should be tapped in the game

---
+++ app/game/grid/grid.component.ts
@@ -0,0 +1,34 @@
+﻿import {Component, ViewChildren,QueryList,EventEmitter} from '@angular/core';
+import {TapSqr} from './sqr';
+@Component({
+    selector: "tap-grid",
+    directives: [TapSqr],
+    templateUrl: './grid.template.html'
+})
+export class TapGrid{
+    missedSqrs = Array<number>();
+    gotSqrs = Array<number>();
+    fireNum = 5;
+    fireTime = 3000;
+    @ViewChildren(TapSqr) sqrList: QueryList<TapSqr>;
+    constructor() {
+    }
+    ngAfterViewInit() {
+        setInterval(() => {
+            for (let i = 0; i < this.fireNum; i++) {
+                let rand: number = Math.floor(Math.random() * this.sqrList.toArray().length)
+                if (this.missedSqrs.indexOf(rand) > -1) {
+                    this.sqrList.toArray()[rand].beginCountDown(rand);
+                } else {
+                    i--;
+                }
+            }
+        }, this.fireTime)
+    }
+    gotIt(event) {
+        this.gotSqrs.push(event[0])
+    }
+    missed(event) {
+        this.missedSqrs.push(event[0]);
+    }
+}
---
+++ app/game/grid/sqr/sqr.component.ts
@@ -0,0 +1,46 @@
+﻿import {Component, Output,EventEmitter} from '@angular/core';
+@Component({
+    selector: "tap-sqr",
+    templateUrl: './sqr.template.html',
+    styleUrls: ['./sqr.style.css']
+})
+export class TapSqr{
+    timeout: any;
+    timeToInitial: number = 1000;
+    timeToMissed: number = 5000;
+    state = "";
+    id:number;
+    @Output() missed = new EventEmitter();
+    @Output() gotIt = new EventEmitter();
+    /// @function TapSqr.beginCountDown
+    /// This will begin countdown. A user must tap the sqr in the time allocated. If no tap in time allocated then fire
+    /// missed event to grid. This will be stored in the grid to rule out this sqr from future calls.
+    /// @param {number} The id of this sqr from the grid.
+    beginCountDown(id) {
+        this.id = id;
+        this.state = "toBeClicked";
+        this.timeout = setTimeout(() => {
+            this.state = "missed";
+            this.missed.next([id]);
+        }, this.timeToMissed)
+
+    }
+    /// @function tapSqr.onClick
+    /// If we need to click this sqr then cancel missed timeout and fire gotIt event. This will let the grid component know
+    /// we have got to this sqr in time. If we hit a sqr not to hit then let the user know my another change in style.
+    onClick() {
+        if (this.state === "toBeClicked") {
+            clearTimeout(this.timeout);
+            this.state = "clicked";
+            this.gotIt.next([this.id]);
+            setTimeout(() => {
+                this.state = "";
+            }, this.timeToInitial)
+        } else {
+            this.state = "nonClick";
+            setTimeout(() => {
+                this.state = "";
+            }, this.timeToInitial)
+        }
+    }
+}
